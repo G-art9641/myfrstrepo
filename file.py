6431188542b9253dfaf48b7da1718deb792f9b3f
manvi <manvi>
2020-01-23 15:12:29 +0100

    commited first time

---
+++ file.py
@@ -0,0 +1,143 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+4+5
+4*44
+
+#%%     (create a cell)
+
+
+#creating integer
+
+#%% 
+
+#%% creating floats
+
+
+3.4
+
+
+
+
+
+
+
+
+
+
+
+
+#%%
+
+"I'm called\"Pepe\", but my name is john"
+
+#%%
+
+
+True
+
+False #STARTING WITH CAPITAL LETTER
+
+#%% 
+
+#getting the type of the value
+
+type(value)
+
+#eg
+type("patata")
+type(True) # returns boolean
+
+#%%
+r =2
+
+
+3.14*r**2
+
+#%%
+r=2
+
+
+(4/3)*3.14*r**3
+
+#%%
+
+"Hello" +"world"
+
+#%%
+
+"Na" * 16 + "Batman"
+
+#%%
+
+age = 55
+
+age * 2
+
+age_of_the_user = 55
+
+AGE = 10
+
+print(AGE)
+print(age)
+
+#%%
+
+print(bool(1))
+
+#%% # user input
+
+name= input ("Tell me your name : ")
+
+print("Hello, " + name)
+
+# in input function user have to write something at console, user is waiting for the input
+
+#%%
+
+age = input("Tell me your age : ")
+year = date.today().year
+
+
+print(2020 - int(age))
+
+
+#%%
+
+print(1*60*60)
+
+2 + (3 + 4) + (5 * 33 ** 34) #prints according to precidence
+#%%
+
+age = input("your age :")
+mothers_age = input("your mother's age : ")
+
+difference = int(mothers_age) -int( age)
+print(difference)
+#%%
+print(3 + int( "3"))
+
+
+#%%
+
+def area_square(side):
+    area = side**2
+    print (area)
+    
+area_square(3)
+
+if type(side) == int or type(side) == float:
+    return side ** 2
+    
+    
+else:
+    print("type must ne int or float")
+    
+are_square_return(")
+
+""""
+@author: kushagra
+""""
